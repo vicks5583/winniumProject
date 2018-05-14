16ff340fa4caf7a06953ec4ce648a69d10ecfe7a
ahmed.siddiqui <ahmed.siddiqui@myboltv.net>
2018-05-14 20:03:28 +0500

    first push with notepad automation script

---
+++ src/main/java/winnium/notepad.java
@@ -0,0 +1,40 @@
+package winnium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.WiniumDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class notepad {
+	 WiniumDriver driver = null;
+	 
+	 @BeforeClass
+	 public void Setup ()
+	 {
+	  DesktopOptions desktop = new DesktopOptions();
+	  desktop.setApplicationPath("C:\\Windows\\System32\\notepad.exe");
+	  try {
+	   driver = new WiniumDriver(new URL ("http://localhost:9999"), desktop);
+	  }catch (MalformedURLException e)
+	  {
+	   e.printStackTrace();
+	  }
+	 }
+	 
+	 @Test
+	 public void testcase ()
+	 {
+	  driver.findElementByClassName("Edit").sendKeys("Kushan's Winnium Test");
+	 }
+	 
+	 @AfterClass 
+	 
+	 public void TearDown()
+	 {
+	  driver.close();
+	 }
+	}
